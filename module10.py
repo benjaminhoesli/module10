688f13be71c8ca7f151c4687641ed6aa26f93c51
benjaminhoesli <73198998+benjaminhoesli@users.noreply.github.com>
2021-04-18 19:49:27 +0200

    Update module10.py
    done

--- module10.py
+++ module10.py
@@ -1,1 +1,35 @@
-"Hello"
+import sys
+x=sys.argv
+
+def encrypt(n):
+    inp=input("enter message")
+    inp=inp.upper()
+    final=""
+    block = ""
+    count=0
+    for ch in inp:
+        if ch in list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+            count+=1
+            ordi=ord(ch)+n
+            if ordi<=90:
+                block+=(chr(ordi))
+            else:
+                block+=(chr(ordi-26))
+            if len(block)>4:
+                final+=(block)
+                final+=" "
+                count+=1
+                block=""
+        if count==59:
+            final+='\n'
+            count=0
+    if len(block)>0:
+        if count+len(block)<59:
+            final+=block
+        else:
+            final+='\n'
+            final+=block
+    return final
+
+
+print(encrypt(int(x[1])))
